1abbd2d9e043cfeddfa3ac093afbffb19d3689a1
Melka Tolessa <melkatole1@gmail.com>
2023-02-27 21:49:26 +0300

    navigation front and backwards

--- scripts.js
+++ scripts.js
@@ -1,49 +1,69 @@
 
+let showAll = false;
+let pages = ["home"];
+let now = 0;
+let last = 0
+checkNavState()
+checkWidth()
+
 function checkWidth(){
     let width = $("main").width();
     let partition1 = $("body main .lists #0 .list .list--part")
     let partition2 = $("body main .lists #1 .list .list--part")
-    $("body main .lists .list--container .list .list--part").css("display", "none")
+    showAll?$("body main .lists .list--container .list .list--part").css("display", "blok"):
+            $("body main .lists .list--container .list .list--part").css("display", "none")
 
     if(width>1080){
-        console.log("hey")
         $("body main .lists .list--container .list").css("grid-template-columns","repeat(5, 1fr)");
-        
-        let i=0
-        while(i<5){
-            partition1[i].style.display = "block"
-            partition2[i].style.display = "block"
-            i++;
+        if(!showAll){
+            let i=0
+            while(i<5){
+                partition1[i].style.display = "block"
+                partition2[i].style.display = "block"
+                i++;
+            }
+        } else {
+            partition1.css("display", "block")
+            partition2.css("display", "block")
         }
     } else if(width>900){
-        console.log("hey")
         $("body main .lists .list--container .list").css("grid-template-columns","repeat(4, 1fr)");
-
-        let i=0
-        while(i<4){
-            partition1[i].style.display = "block"
-            partition2[i].style.display = "block"
-            i++;
+        if(!showAll){
+            let i=0
+            while(i<4){
+                partition1[i].style.display = "block"
+                partition2[i].style.display = "block"
+                i++;
+            }
+        } else {
+            partition1.css("display", "block")
+            partition2.css("display", "block")
         }
     } else if(width>700){
-        console.log("hey")
         $("body main .lists .list--container .list").css("grid-template-columns","repeat(3, 1fr)");
-        
-        let i=0
-        while(i<3){
-            partition1[i].style.display = "block"
-            partition2[i].style.display = "block"
-            i++;
+        if(!showAll){
+            let i=0
+            while(i<3){
+                partition1[i].style.display = "block"
+                partition2[i].style.display = "block"
+                i++;
+            }
+        } else {
+            partition1.css("display", "block")
+            partition2.css("display", "block")
         }
     } else {
-        console.log("hey")
         $("body main .lists .list--container .list").css("grid-template-columns","repeat(2, 1fr)");
-        
-        let i=0
-        while(i<2){
-            partition1[i].style.display = "block"
-            partition2[i].style.display = "block"
-            i++;
+        if(!showAll){
+            let i=0
+            while(i<2){
+                partition1[i].style.display = "block"
+                partition2[i].style.display = "block"
+                i++;
+            }
+        } else {
+            partition1.css("display", "block")
+            partition2.css("display", "block")
         }
     } 
 }
@@ -52,26 +72,74 @@ window.addEventListener("resize", checkWidth)
 
 function handleScroll(event){
     let posY = $("main").scrollTop()
-    console.log($("main").scrollTop())
+    // console.log($("main").scrollTop())
     if(posY<600){
         $("body main .top--bar").css("background-color",`rgba(16, 16, 16, ${0.7+posY*0.0015})`)
     }
 
 }
-let last = 0
+
 function handleDrag(event){
-    console.log(event.pageX, event.pageY, "hey")
     let width = (event.pageX!=0?event.pageX:last)
     if(event.pageX<=390&&event.pageX>=130){
         $("body").css("grid-template-columns", `${width}px 1fr`)
     }
     last = event.pageX
-    console.log($("main").innerWidth())
     checkWidth();
 }
-
-checkWidth()
 
 $("form").click(function(e){
     e.preventDefault();
 })
+
+function handleShowAll(id){
+    showAll = true
+    if(!pages[1]){
+        pages.push(id)
+        now++;
+    } else {
+        pages[1] = id
+    }
+    checkWidth();
+    $(".list--container").css("display", "none")
+    $(`#${id}`).css("display", "block")
+    $(`#${id} .list--header`).css("display", "none")
+    console.log(pages, now, pages[now])
+    checkNavState()
+}
+
+function handleNavigation(page){
+    now+=page;
+    if(now < 0 || now > pages.length-1){
+        now -= page;
+    }
+
+    if(now == 0){
+        showAll = false
+    }
+
+    if(pages[now] == "home"){
+        $(".list--container").css("display", "block")
+        $(`.list--container .list--header`).css("display", "flex")
+    } else {
+        handleShowAll(pages[now])
+    }
+
+    checkWidth()
+    checkNavState()
+    console.log(pages, now, pages[now])
+}
+
+function checkNavState(){
+    if(now == 0){
+        $("#prev:hover").css("cursor", "not-allowed")
+    } else {
+        $("#prev:hover").css("cursor", "pointer")
+    }
+
+    if(pages.length-1<now+1){
+        $("#next:hover").css("cursor", "not-allowed")
+    } else {
+        $("#next:hover").css("cursor", "pointer")
+    }
+}
